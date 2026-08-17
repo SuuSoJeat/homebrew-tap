588ffbcab806f2fbad427cdcd205cb1ade45e336
Socheat Sou <suusojeat@Eterna-MBP-64.local>
2026-08-17 11:53:55 +0700

    Update Orbit to v0.3.0

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -1,12 +1,12 @@
 class Orbit < Formula
   desc "iCloud-first project context CLI"
   homepage "https://github.com/SuuSoJeat/orbit"
-  url "https://github.com/SuuSoJeat/orbit/releases/download/v0.2.1/orbit-0.2.1.tar.gz"
-  sha256 "1d0da51d7ff58b8bdbefa36470b89dd0e0e04a3014fb2e25dc25f2931e9a2e6f"
+  url "https://github.com/SuuSoJeat/orbit/releases/download/v0.3.0/orbit-0.3.0.tar.gz"
+  sha256 "9648be4ad003cf8628d6218cb6813fa3bdbb7a4c5db5397e4d3cb281feb2c19b"
 
   def install
     libexec.install "bin", "templates", "VERSION", "README.md", "Makefile"
-    (bin/"orbit").write_exec_script libexec/"bin/orbit"
+    bin.install_symlink libexec/"bin/orbit"
   end
 
   test do
