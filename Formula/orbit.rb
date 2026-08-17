4f85d8b58b0411825b3e9f9d8eca233c5232eb89
Socheat Sou <suusojeat@Eterna-MBP-64.local>
2026-08-17 13:41:17 +0700

    Update Orbit formula to 0.3.2

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -1,8 +1,8 @@
 class Orbit < Formula
   desc "iCloud-first project workspace CLI"
   homepage "https://github.com/SuuSoJeat/orbit"
-  url "https://github.com/SuuSoJeat/orbit/releases/download/v0.3.1/orbit-0.3.1.tar.gz"
-  sha256 "e3e13addb674f20aef463289408aa74526184d1f30b69b104e736ffb50379bf1"
+  url "https://github.com/SuuSoJeat/orbit/releases/download/v0.3.2/orbit-0.3.2.tar.gz"
+  sha256 "237e8bedd4d99178c7bab3525233bd7491754d202c46d4b973d379b36ae25a2d"
 
   def install
     libexec.install "bin", "templates", "VERSION", "README.md", "Makefile"
